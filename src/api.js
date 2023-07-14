66b9f09e5e0c05f4a93c81ba1ffb135934083bd6
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-14 23:51:15 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,29 +2,17 @@ import axios from 'axios';
 
 const baseURL = 'https://64aff9d5c60b8f941af4fe46.mockapi.io';
 
-export const fetchContactsAPI = async () => {
-  try {
-    const response = await axios.get(`${baseURL}/contacts`);
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to fetch contacts');
-  }
+export const fetchContactsAPI = () => {
+  return axios.get(`${baseURL}/contacts`)
+    .then((response) => response.data);
 };
 
-export const addContactAPI = async (contact) => {
-  try {
-    const response = await axios.post(`${baseURL}/contacts`, contact);
-    return response.data;
-  } catch (error) {
-    throw new Error('Failed to add contact');
-  }
+export const addContactAPI = (contact) => {
+  return axios.post(`${baseURL}/contacts`, contact)
+    .then((response) => response.data);
 };
 
-export const deleteContactAPI = async (contactId) => {
-  try {
-    await axios.delete(`${baseURL}/contacts/${contactId}`);
-    return contactId;
-  } catch (error) {
-    throw new Error('Failed to delete contact');
-  }
+export const deleteContactAPI = (contactId) => {
+  return axios.delete(`${baseURL}/contacts/${contactId}`)
+    .then(() => contactId);
 };
