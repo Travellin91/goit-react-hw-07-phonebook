392089e9896fa3b3ce997a7b4e6dd84b51e4bc44
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-14 23:56:19 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import filterReducer from './filterSlice';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
 
 export default store;
