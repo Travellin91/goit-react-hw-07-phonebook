fa22820f67e2569058466f8e8553c5f5ca2aa17f
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-07-15 13:01:28 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,23 +1,49 @@
-import axios from 'axios';
-
 const baseURL = 'https://64aff9d5c60b8f941af4fe46.mockapi.io';
 
 export const fetchContactsAPI = () => {
-  return axios.get(`${baseURL}/contacts`).then(response => {
-    if (Array.isArray(response.data)) {
-      return response.data;
-    } else {
-      throw new Error('Invalid response format');
-    }
-  });
+  return fetch(`${baseURL}/contacts`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch contacts');
+      }
+    })
+    .then(data => {
+      if (Array.isArray(data)) {
+        return data;
+      } else {
+        throw new Error('Invalid response format');
+      }
+    });
 };
 
 export const addContactAPI = contact => {
-  return axios
-    .post(`${baseURL}/contacts`, contact)
-    .then(response => response.data);
+  return fetch(`${baseURL}/contacts`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(contact)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to add contact');
+      }
+    });
 };
 
 export const deleteContactAPI = contactId => {
-  return axios.delete(`${baseURL}/contacts/${contactId}`).then(() => contactId);
+  return fetch(`${baseURL}/contacts/${contactId}`, {
+    method: 'DELETE'
+  })
+    .then(response => {
+      if (response.ok) {
+        return contactId;
+      } else {
+        throw new Error('Failed to delete contact');
+      }
+    });
 };
